4ce82035e1fef9b1f330865c055b2fcf74f4aee1
FanDoubleLucky <38746998+FanDoubleLucky@users.noreply.github.com>
2018-11-05 13:22:57 +0800

    resNet

---
+++ resNet_CIFAR.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  5 12:49:49 2018
+
+@author: FYZ
+"""
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.functional as F
+
+class ResNet(nn.Module):
+    def __init__(self):
+        super(ResNet,self).__init__
+        self.conv1 = nn.Conv2d(3,64,3)
+        self.layer1 = self.make_layer()
+        self.layer2 = self.make_layer()
+        self.layer3 = self.make_layer()
+        self.layer4 = self.make_layer()
+        self.fc1  = nn.Linear(512,10)
+        
+    def make_layer(self,core_size,in_channel,out_channel):
+        layer = []
+        
+        return nn.Sequential(*layer)
+    
+    def forward(self,x):
+        x = F.relu(self.conv1(x))
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.fc1(x)
+        return x
